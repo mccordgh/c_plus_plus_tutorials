9c7a21563ccf73ace29fec0fc0a3ac22b7291d08
Matthew McCord <mattm.eternal@gmail.com>
2018-09-23 21:30:00 -0700

    User Input

---
+++ user_input/user_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name, dummy: ";
+    cin >> name;
+
+    cout << "I'm sorry I called you a dummy, " << name;
+
+    return 0;
+}
